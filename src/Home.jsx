9122b18767fb1ffe6ce5f2d8da73e7c308f16280
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-21 10:03:42 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className=" w-1/2 m-auto mt-10">
+        <h1 className="text-4xl text-red-200 mb-2">Home</h1>
+        <p className="mb-2 text-zinc-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptas atque aliquam accusantium velit aspernatur consectetur saepe aperiam voluptates incidunt, voluptatum adipisci ipsa sed repudiandae.</p> 
+
+        <button className="p-5 text-white bg-zinc-400">Explore more</button>   
+        </div>
+    )
+}
+
+export default Home;
