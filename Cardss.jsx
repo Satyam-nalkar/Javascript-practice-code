8a7d35fcc7bbbc21790d8a3917012655e2c2ddf6
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-24 09:18:39 +0530

    Add files via upload

---
+++ Cardss.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cards from "./Cards";
+function Cardss(){
+    return(
+   
+        <div className="w-full max-h-72 overflow-auto p-4 flex justify-center gap-4 flex-wrap">
+            <Cards/>
+           
+        </div>
+    )
+}
+
+export default Cardss;
