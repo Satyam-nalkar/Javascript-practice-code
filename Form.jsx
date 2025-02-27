590bc78bcb93ec67ae81c84e382f31a630acd14f
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-02-27 10:07:25 +0530

    Add files via upload

---
+++ Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";  
+
+function Form(){
+    return(
+    
+        <div className="mt-10 flex gap-10 justify-center">
+          <form className="flex gap-10" action="">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+          </form>
+        </div>
+    )
+}
+
+export default Form;
