a3c878d456ed3b754b949984408aaa785e1f561b
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-03-07 10:05:07 +0530

    Add files via upload

---
+++ App3.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { useForm } from "react-hook-form";
+
+
+function App3(){
+const {register ,handleSubmit} = useForm();
+
+    return (
+        <div>
+     <form action="" onSubmit={handleSubmit(data=>console.log(data))}>
+        <input {...register('name')} type="text" placeholder="name"/>
+        <input {...register('email')} type="email" placeholder="email"/>
+        <input type="submit" />
+     </form>
+     
+        </div>
+    )
+} 
+
+export default App3;
+
+
+
