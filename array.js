2f5b3e78a3b6e4d60f0b68548dbb89af7f082e1e
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-03-12 10:00:44 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+// var users=["harsh","harshika","harshita","akansha","nishi"];
+// // console.log(users[5]);
+// var arr=[1,2,"3",function(){},[],4];
+ var arr =[1,2,3,4,5];
+ arr.forEach(function(val){
+    console.log(val + 1);
+    console.log(val);
+ })
