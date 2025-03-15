65168cd57a7cb88bfa969c6832095f9febe78c66
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-03-15 19:30:24 +0530

    Add files via upload

---
+++ custom_evnts.js
@@ -0,0 +1,12 @@
+// const evt = new Event("chacha");
+// document.querySelector("button")
+// .addEventListener("chacha",function(){
+//     alert("chacha event hua");
+// }) 
+// document.querySelector("button").dispatchEvent(evt);
+const yourevent = new Event("gharjaao");
+document.querySelector("button")
+.addEventListener("gharjaao",function(){
+    alert("jaa raha hu");
+})
+document.querySelector("button").dispatchEvent(yourevent);
