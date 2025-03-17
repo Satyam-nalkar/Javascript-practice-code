22aecfbd95087fff8a5a267361d6b664d06866df
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-03-17 09:29:19 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+// function abcd(){
+//     var i =12;
+//     console.log(i);
+//     for(var b=12;b<22;b++){
+//         console.log("hello");
+//     }
+// }
+// abcd();
+// var dateofbirth = 7;
+// var todaysdate =7;
+// function happybirthday(){
+//     console.log("happy birthday");
+// }
+// if(dateofbirth===todaysdate)
+// {
+//     happybirthday();
+// } 
+function abcd(val){
+    console.log(12+val);
+}
+abcd(12);
