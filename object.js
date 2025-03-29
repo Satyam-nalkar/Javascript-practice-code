3087661e97c87b0985b751f5b6db08d5ad2a0165
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-03-29 21:12:15 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,15 @@
+// var obj = {
+//     name: "harsh",
+//     age:25,
+//     email:"hi hi hi",
+//     contact:728732633
+   
+// };
+
+var battery = {
+    compony:"canon",
+    price:1200,
+    for:"camera",
+    isWorking:true
+}
+battery.compony
